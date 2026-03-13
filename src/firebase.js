e410ef07f2636b2a35a30577cb6de6f22b3507a6
CLaireLise-dev <clairedemettre@gmail.com>
2026-03-13 13:35:01 +0100

    Initialisation du projet, mise en place de firebase et premier design page accueil

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBL4h1tXGsB36GuyshXINxMmPPXw5G53a0",
-  authDomain: "clonex-421e0.firebaseapp.com",
-  projectId: "clonex-421e0",
-  storageBucket: "clonex-421e0.firebasestorage.app",
-  messagingSenderId: "113895862804",
-  appId: "1:113895862804:web:b4c2e57ce5bb8158bfed01",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
 export default app;
