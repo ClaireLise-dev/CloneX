3d5474ba1a13a7b9062eae189c170dde0c6dff24
CLaireLise-dev <clairedemettre@gmail.com>
2026-03-22 11:25:23 +0100

    Rajout d'un spinner pour le loading

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,8 +1,15 @@
 import { Outlet } from "react-router-dom";
+import { Bars } from "react-loader-spinner";
 
 export default function Main() {
   return (
     <>
+      <div
+        data-theme="lightX"
+        className="flex items-center justify-center h-screen"
+      >
+        <Bars color="#7c3aed" />
+      </div>
       <div>
         <Outlet />
       </div>
