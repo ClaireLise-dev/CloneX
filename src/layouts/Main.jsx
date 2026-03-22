6c19ea45658bbf0b996c567573e6f2a0f609087e
CLaireLise-dev <clairedemettre@gmail.com>
2026-03-22 11:30:09 +0100

    Ajout du contexte pour le spinner

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,15 +1,23 @@
 import { Outlet } from "react-router-dom";
 import { Bars } from "react-loader-spinner";
+import { useContext } from "react";
+import { AuthContext } from "../store/AuthProvider";
 
 export default function Main() {
-  return (
-    <>
+  const { user, loading } = useContext(AuthContext);
+
+  if (loading) {
+    return (
       <div
         data-theme="lightX"
         className="flex items-center justify-center h-screen"
       >
         <Bars color="#7c3aed" />
       </div>
+    );
+  }
+  return (
+    <>
       <div>
         <Outlet />
       </div>
